fdd40be0c4283565c60914724e1c5e354e07bb0b
Svyatoslau <svyatyar2@gmail.com>
2021-04-21 22:39:03 +0300

    feat: start data

---
+++ simplex-method/data.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+countClusters = 3
+countStations = 10
+stationsInClastors = np.array([4,6,10],int)
+processingTimes = np.array([10,4,8,6,2,3,8,2,6,10],float)
+stationRestrictions = np.array([700,700,700,700,700,700,700,700,700,700],float)
+totalVolume = 1000
+clastorsRestrictions = np.array([400,800,300])
